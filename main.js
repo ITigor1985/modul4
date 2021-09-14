1e2113fda866385f6f9cb57fe0e79894bf0fefc2
Igor Tarasov <xromer85@gmail.com>
2021-09-14 17:25:02 +0300

    23

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 //--------------------------------------------#2-----------------------
 // Дополни функцию makeMessage так, чтобы она ожидала вторым параметром (параметр callback) колбэк-функцию и возвращала ее вызов. Функция deliverPizza или makePizza будет передаваться как колбэк и ожидать аргументом имя готовой доставляемой пиццы.
-    // function deliverPizza(pizzaName) {
-    //     return `Delivering ${pizzaName} pizza.`;
-    // }
-    
-    // function makePizza(pizzaName) {
-    //     return `Pizza ${pizzaName} is being prepared, please wait...`;
-    // }
-    
-    // // Chande code below this line
-    // function makeMessage(pizzaName, callback) {
-    //     return callback(pizzaName);
-    // }
+// function deliverPizza(pizzaName) {
+//     return `Delivering ${pizzaName} pizza.`;
+// }
 
-    
-    // console.log(makeMessage("Selo", makePizza));
+// function makePizza(pizzaName) {
+//     return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
+
+// // Chande code below this line
+// function makeMessage(pizzaName, callback) {
+//     return callback(pizzaName);
+// }
+
+// console.log(makeMessage("Selo", makePizza));
 //------------------------------------------------------------------------------------
 //-----------------------------------------#3-----------------------------------------
 // Дополни второй вызов функции makePizza(pizzaName, callback), передав вторым аргументом инлайн колбэк-функцию eatPizza(pizzaName), которая логирует строку "Eating pizza <имя пиццы>".
@@ -22,12 +21,12 @@
 //     console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
 //     callback(pizzaName);
 //   }
-  
+
 //   makePizza("Royal Grand", function deliverPizza(pizzaName) {
 //     console.log(`Delivering pizza ${pizzaName}.`);
 //   });
 //   // Change code below this line
-  
+
 //   makePizza("Ultracheese", function eatPizza(pizzaName){
 //             console.log(`Eating pizza ${pizzaName}`)});
 //----------------------------------------------------------------------------------
@@ -36,7 +35,7 @@
 
 // Если в свойстве pizzas нет пиццы с названием из параметра pizzaName, метод order должен возвращать результат вызова колбэка onError, передавая ему аргументом строку "There is no pizza with a name <имя пиццы> in the assortment."
 // Если в свойстве pizzas есть пицца с названием из параметра pizzaName, метод order должен возвращать результат вызова колбэка onSuccess, передавая ему аргументом имя заказанной пиццы.
-// После объявления объекта pizzaPalace мы добавили колбэки и вызовы методов. Пожалуйста ничего там не меняй.  
+// После объявления объекта pizzaPalace мы добавили колбэки и вызовы методов. Пожалуйста ничего там не меняй.
 // const pizzaPalace = {
 //     pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
 //     order(pizzaName, onSuccess, onError) {
@@ -52,17 +51,17 @@
 //     },
 //   };
 //   // Change code above this line
-  
+
 //   // Callback for onSuccess
 //   function makePizza(pizzaName) {
 //     return `Your order is accepted. Cooking pizza ${pizzaName}.`;
 //   }
-  
+
 //   // Callback for onError
 //   function onOrderError(error) {
 //     return `Error! ${error}`;
 //   }
-  
+
 //   // Method calls with callbacks
 //   console.table(pizzaPalace.pizzas);
 //   console.log(pizzaPalace.order('Smoked', makePizza, onOrderError));
@@ -77,7 +76,7 @@
 // function calculateTotalPrice(orderedItems) {
 //     let totalPrice = 0;
 //     // Change code below this line
-  
+
 //     //method 1
 //     // orderedItems.forEach(function(item, i){
 //     //   totalPrice += orderedItems[i]
@@ -87,7 +86,7 @@
 //     orderedItems.forEach ((item, i) =>
 //         totalPrice += orderedItems[i]
 //     );
-//-------------------------------------------------  
+//-------------------------------------------------
 //     // Change code above this line
 //     return totalPrice;
 //   }
@@ -102,21 +101,21 @@
 
 // function filterArray(numbers, value) {
 //     const filteredNumbers = [];
-    // Change code below this line
-//--------------------------Method1-----------------------------------  
-    // for (let i = 0; i < numbers.length; i += 1) {
-    //   if (numbers[i] > value) {
-    //     filteredNumbers.push(numbers[i]);
-    //   }
-    // }
-//--------------------------Method2----------------------------------- 
-    // numbers.forEach(function(item, i){
-    //     if (item > value) {
-    //              filteredNumbers.push(numbers[i]);
-    //          }
-    // });
-//-------------------------------------------------------------------- 
-    // Change code above this line
+// Change code below this line
+//--------------------------Method1-----------------------------------
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] > value) {
+//     filteredNumbers.push(numbers[i]);
+//   }
+// }
+//--------------------------Method2-----------------------------------
+// numbers.forEach(function(item, i){
+//     if (item > value) {
+//              filteredNumbers.push(numbers[i]);
+//          }
+// });
+//--------------------------------------------------------------------
+// Change code above this line
 //     return filteredNumbers;
 //   }
 //   console.log(value);
@@ -131,24 +130,23 @@
 
 // function getCommonElements(firstArray, secondArray) {
 //     const commonElements = [];
-    // Change code below this line
- //---------------------------Method1(for) ---------------
-    // for (let i = 0; i < firstArray.length; i += 1) {
-    //   if (secondArray.includes(firstArray[i])) {
-    //     commonElements.push(firstArray[i]);
-    //   }
-    // }
-    //------------------Method2(forEach)------------
-    // firstArray.forEach(function(item){
-    //     if (secondArray.includes(item)) {
-    //             commonElements.push(item);
-    //           }
-    // })
-    //------------------------------------------------
+// Change code below this line
+//---------------------------Method1(for) ---------------
+// for (let i = 0; i < firstArray.length; i += 1) {
+//   if (secondArray.includes(firstArray[i])) {
+//     commonElements.push(firstArray[i]);
+//   }
+// }
+//------------------Method2(forEach)------------
+// firstArray.forEach(function(item){
+//     if (secondArray.includes(item)) {
+//             commonElements.push(item);
+//           }
+// })
+//------------------------------------------------
 
-  
-    // return commonElements;
-    // Change code above this line
+// return commonElements;
+// Change code above this line
 //   }
 //   console.log(getCommonElements(array1, array2));
 //-------------------------------------------------------------------------------------
@@ -167,7 +165,7 @@
 //-----------------------------implicit return(#9)-------------------------------------
 //const calculateTotalPrice = (quantity, pricePerItem) => quantity * pricePerItem;
 //---------------------------------------------------------------------------------
-  
+
 //   console.log(calculateTotalPrice(23, 5));
 //-----------------------------------------------------------------------------------------------
 //-------------------------------------------#10-------------------------------------------------
@@ -177,23 +175,22 @@
 //---------------------------------------------------------------------------------------------
 // function calculateTotalPrice(orderedItems) {
 //     let totalPrice = 0;
-  
+
 //     orderedItems.forEach(function (item) {
 //       totalPrice += item;
 //     });
-  
+
 //     return totalPrice;
 //   }
 //-----------------------------------------------------------------------------------------------
 // const calculateTotalPrice = (orderedItems) => {
 //         let totalPrice = 0;
-  
+
 //     orderedItems.forEach((item) => {
 //       totalPrice += item;
 //     });
 //     return totalPrice;
 // }
-
 
 //   console.log(calculateTotalPrice(array1));
 //--------------------------------------------------------------------------------------------------
@@ -202,26 +199,26 @@
 //--------------------------------------------------------------------------------------------------
 // function filterArray(numbers, value) {
 //     const filteredNumbers = [];
-  
+
 //     numbers.forEach(function (number) {
 //       if (number > value) {
 //         filteredNumbers.push(number);
 //       }
 //     });
-  
+
 //     // Change code above this line
 //     return filteredNumbers;
 //   }
-//--------------------------------------------------------------------------------------------------  
+//--------------------------------------------------------------------------------------------------
 // const filterArray = (numbers, value) => {
 //     const filteredNumbers = [];
-  
+
 //     numbers.forEach((number) => {
 //       if (number > value) {
 //         filteredNumbers.push(number);
 //       }
 //     });
-  
+
 //     // Change code above this line
 //     return filteredNumbers;
 //   }
@@ -230,30 +227,30 @@
 //Замени объявление функции getCommonElements() и коллбек для метода forEach() на стрелочные функции.
 // function getCommonElements(firstArray, secondArray) {
 //     const commonElements = [];
-  
+
 //     firstArray.forEach(function (element) {
 //       if (secondArray.includes(element)) {
 //         commonElements.push(element);
 //       }
 //     });
-  
+
 //     // Change code above this line
 //     return commonElements;
 //   }
-//-------------------------------------------------------------------------------------------------  
+//-------------------------------------------------------------------------------------------------
 // const getCommonElements = (firstArray, secondArray) => {
 //     const commonElements = [];
-  
+
 //     firstArray.forEach((element) => {
 //       if (secondArray.includes(element)) {
 //         commonElements.push(element);
 //       }
 //     });
-  
+
 //     // Change code above this line
 //     return commonElements;
 //   }
-  //------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
 //-------------------------------------------#13---------------------------------------------------
 // Функция changeEven(numbers, value) принимает массив чисел numbers и обновляет каждый элемент, значение которого это чётное число, добавляя к нему значение параметра value.
 
@@ -267,7 +264,7 @@
 //     }
 //     // Change code above this line
 //   }
-  //---------------------------------------------------------------------
+//---------------------------------------------------------------------
 //   const numbers = [1, 2, 3, 4, 5];
 
 //   const changeEven = (numbers, value) => {
@@ -281,7 +278,7 @@
 
 //     })
 //     return array;
-    // Change code above this line
+// Change code above this line
 //   }
 
 //   const doubledNumbers = changeEven(numbers, 10);
@@ -289,7 +286,7 @@
 // Не произошло мутации исходных данных
 // console.log(numbers); // [1, 2, 3, 4, 5]
 // Функция вернула новый массив с изменёнными данными
-// console.log(doubledNumbers); 
+// console.log(doubledNumbers);
 //--------------------------------------------------------------------------------------------------
 //-------------------------------------------#14(map())-----------------------------------------------------
 // Дополни код так, чтобы в переменной planetsLengths получился массив длин названий планет. Обязательно используй метод map().
@@ -324,7 +321,7 @@
 //     { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
 //   ];
 //   // Change code below this line
-  
+
 //   const titles = books.map(book => book.title);
 //   console.log(titles);
 //----------------------------------------------------------------------------------------------------------
@@ -348,23 +345,21 @@
 //     },
 //   ];
 //   // Change code below this line
-  
+
 //   const genres = books.flatMap(book => book.genres);
 //   console.log(genres);
 //--------------------------------------------------------------------------------------------------------------------
 //----------------------------------------------#17-----------------------------------------------------------------
 //Дополни функцию getUserNames(users) так, чтобы она возвращала массив имён пользователей (свойство name) из массива объектов в параметре users.
 
-const getUserNames = users => {
-    
-    const usersArray = users.map(user => user.name);
-      return usersArray;
+const getUserNames = (users) => {
+    const usersArray = users.map((user) => user.name);
+    return usersArray;
 };
 //-------------------------------------------------------------------------------------------------------------------
 //---------------------------------------------#18-------------------------------------------------------------------
 //Дополни функцию getUserEmails(users) так, чтобы она возвращала массив почтовых адресов пользователей (свойство email) из массива объектов в параметре users.
 // const getUserEmails = users => {
-    
 
 //     const emailArray = users.map(user => user.email)
 //     return emailArray;
@@ -378,3 +373,157 @@ const getUserNames = users => {
 // const evenNumbers = numbers.filter(number => number % 2 === 0);
 // const oddNumbers = numbers.filter(number => number % 2 !== 0);
 //-------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------#20-----------------------------------------------------------------
+// Дополни код так, чтобы в переменной allGenres был массив всех жанров книг (свойство genres) из массива books, а в переменной uniqueGenres массив уникальных жанров - без повторений.
+
+// const books = [{
+//         title: "The Last Kingdom",
+//         author: "Bernard Cornwell",
+//         genres: ["adventure", "history"],
+//     },
+//     {
+//         title: "Beside Still Waters",
+//         author: "Robert Sheckley",
+//         genres: ["fiction", "mysticism"],
+//     },
+//     {
+//         title: "Redder Than Blood",
+//         author: "Tanith Lee",
+//         genres: ["horror", "mysticism", "adventure"],
+//     },
+// ];
+// // Change code below this line
+// const allGenres = books.flatMap((book) => book.genres);
+// const uniqueGenres = allGenres.filter(
+//     (genre, index, array) => array.indexOf(genre) === index
+// );
+
+// console.log(allGenres); //['adventure', 'history', 'fiction', 'mysticism', 'horror', 'mysticism', 'adventure']
+// console.log(uniqueGenres);//['adventure', 'history', 'fiction', 'mysticism', 'horror']
+//-----------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------#21(filter())------------------------------------------------------------------------
+// Используя метод filter() дополни код так, чтобы:
+
+// В переменной topRatedBooks получился массив книг рейтинг которых (свойство rating) больше либо равно значению переменной MIN_RATING.
+// В переменной booksByAuthor получился массив книг написанных автором с именем (свойство author) которое совпадает со значением в переменной AUTHOR.
+
+// const books = [{
+//         title: "The Last Kingdom",
+//         author: "Bernard Cornwell",
+//         rating: 8.38,
+//     },
+//     {
+//         title: "Beside Still Waters",
+//         author: "Robert Sheckley",
+//         rating: 8.51,
+//     },
+//     {
+//         title: "The Dream of a Ridiculous Man",
+//         author: "Fyodor Dostoevsky",
+//         rating: 7.75,
+//     },
+//     { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//     { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+
+// const MIN_RATING = 8;
+// const AUTHOR = "Bernard Cornwell";
+// // Change code below this line
+
+// const topRatedBooks = books.filter((book) => book.rating >= MIN_RATING);
+// const booksByAuthor = books.filter((book) => book.author === AUTHOR);
+
+// console.table(topRatedBooks);
+// console.log(booksByAuthor);
+//----------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------#22(filter())------------------------------------------------------------
+//Дополни функцию getUsersWithEyeColor(users, color) так, чтобы она возвращала массив пользователей у которых цвет глаз (свойство eyeColor) совпадает со значением параметра color.
+const array = [{
+        name: "Moore Hensley",
+        email: "moorehensley@indexia.com",
+        eyeColor: "blue",
+        friends: ["Sharron Pace"],
+        isActive: false,
+        balance: 2811,
+        gender: "male",
+    },
+    {
+        name: "Sharlene Bush",
+        email: "sharlenebush@tubesys.com",
+        eyeColor: "blue",
+        friends: ["Briana Decker", "Sharron Pace"],
+        isActive: true,
+        balance: 3821,
+        gender: "female",
+    },
+    {
+        name: "Ross Vazquez",
+        email: "rossvazquez@xinware.com",
+        eyeColor: "green",
+        friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+        isActive: false,
+        balance: 3793,
+        gender: "male",
+    },
+    {
+        name: "Elma Head",
+        email: "elmahead@omatom.com",
+        eyeColor: "green",
+        friends: ["Goldie Gentry", "Aisha Tran"],
+        isActive: true,
+        balance: 2278,
+        gender: "female",
+    },
+    {
+        name: "Carey Barr",
+        email: "careybarr@nurali.com",
+        eyeColor: "blue",
+        friends: ["Jordan Sampson", "Eddie Strong"],
+        isActive: true,
+        balance: 3951,
+        gender: "male",
+    },
+    {
+        name: "Blackburn Dotson",
+        email: "blackburndotson@furnigeer.com",
+        eyeColor: "brown",
+        friends: ["Jacklyn Lucas", "Linda Chapman"],
+        isActive: false,
+        balance: 1498,
+        gender: "male",
+    },
+    {
+        name: "Sheree Anthony",
+        email: "shereeanthony@kog.com",
+        eyeColor: "brown",
+        friends: ["Goldie Gentry", "Briana Decker"],
+        isActive: true,
+        balance: 2764,
+        gender: "female",
+    },
+];
+// const getUsersWithEyeColor = (users, color) => {
+//     let usersArray = [];
+//     usersArray = users.filter((user) => user.eyeColor === color);
+//     return usersArray;
+// };
+// console.log(getUsersWithEyeColor(array, "brown"));
+//----------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------#23----------------------------------------------------------------------
+//Дополни функцию getUsersWithAge(users, minAge, maxAge) так, чтобы она возвращала массив пользователей, возраст которых (свойство age) попадает в промежуток от minAge до maxAge.
+// const getUsersWithAge = (users, minAge, maxAge) => {
+//   let usersAge = [];
+//   usersAge = users.filter((user) => user.age >= minAge && user.age <= maxAge);
+//   return usersAge;
+// };
+//----------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------#24---------------------------------------------------------------------------
+// Дополни функцию getUsersWithFriend(users, friendName) так, чтобы она возвращала массив пользователей у которых есть друг с именем в параметре friendName. Массив друзей пользователя хранится в свойстве friends.
+const getUsersWithFriend = (users, friendName) => {
+    let listFriends = [];
+    for (let user in users) {
+        listFriends = users.friends.filter((friend) => friend === friendName);
+    }
+    return listFriends;
+};
+console.log(getUsersWithFriend(array, "Goldie Gentry"));
